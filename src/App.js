7c73121184b62bfe322f12db9009907d27a2b55e
XVII <karshibaevnurmuhammad@gmail.com>
2023-06-10 01:42:16 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header.js";
+import Home from "./Pages/Home/Home.js";
+import Quiz from "./Pages/Quiz/Quiz.js";
+import Result from "./Pages/Result/Result.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questions, setQuestions] = useState();
+    const [name, setName] = useState();
+    const [score, setScore] = useState(0);
+
+    const fetchQuestions = async (category = "", difficulty = "") => {
+        const { data } = await axios.get(
+            `https://opentdb.com/api.php?amount=10${
+                category && `&category=${category}`
+            }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+        );
+
+        setQuestions(data.results);
+    };
+
+    return (
+        <>
+            <div
+                className='app'
+                style={{ backgroundImage: 'url("/ques2.jpg")' }}>
+                <Header />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <Home
+                                name={name}
+                                setName={setName}
+                                fetchQuestions={fetchQuestions}
+                            />
+                        }
+                    />
+
+                    <Route
+                        path='/quiz'
+                        element={
+                            <Quiz
+                                name={name}
+                                questions={questions}
+                                score={score}
+                                setScore={setScore}
+                                setQuestions={setQuestions}
+                            />
+                        }
+                    />
+                    <Route
+                        path='/result'
+                        element={<Result name={name} score={score} />}
+                    />
+
+                    <Route path='*' element={<h1>404 Not Found</h1>} />
+                </Routes>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div
+            style={{
+                textAlign: "center",
+                marginBottom: 10,
+            }}>
+            by{" "}
+            <a href='https://the17sun.vercel.app' style={{ cursor: "pointer" }}>
+                XVII
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
