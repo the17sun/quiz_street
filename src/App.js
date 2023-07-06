4ac2bd28b3001ee9662fa80c0d4183652414ad27
XVII <karshibaevnurmuhammad@gmail.com>
2023-07-06 17:49:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,88 @@
-import axios from "axios";
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import { Navbar, Header, Main, Production, Services, About, News, Footer, Partners } from "./components";
+
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header.js";
-import Home from "./Pages/Home/Home.js";
-import Quiz from "./Pages/Quiz/Quiz.js";
-import Result from "./Pages/Result/Result.js";
 
-function App() {
-    const [questions, setQuestions] = useState();
-    const [name, setName] = useState();
-    const [score, setScore] = useState(0);
+const App = () => {
+  return (
+    <div className="font-roboto" style={{ fontFamily: "Roboto" }}>
+      <BrowserRouter>
+        {/* <div className="fixed right-5 bottom-5" style={{ zIndex: "1000" }}>
+          <FiSettings />
+        </div> */}
+        <section id="shop" className="relative bg-[url('/src/data/Blur.jpg')] bg-cover 
+        h-[850px] 
+        sm:h-[1000px]
+        md:h-[108vh]
+        lg:h-[108vh]
+        xl:h-[108vh]
+        fullHD:h-[85vh]
+        ">
+          <div className="absolute w-full h-full bg-[#aaaab426] z-0" />
+          <div className="relative w-full h-full 
+            md:py-[10px] md:px-[10px] lg:py-[10px] lg:px-[40px] xl:py-[15px] xl:px-[80px] fullHD:py-[25px] fullHD:px-[280px] z-10">
+            <Navbar />
+            <Main />
+          </div>
+        </section>
 
-    const fetchQuestions = async (category = "", difficulty = "") => {
-        const { data } = await axios.get(
-            `https://opentdb.com/api.php?amount=10${
-                category && `&category=${category}`
-            }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
-        );
+        <section id="production" className="relative bg-white w-full 
+          md:py-[15px] md:px-[10px] lg:py-[15px] lg:px-[40px] xl:py-[25px] xl:px-[80px] 
+          fullHD:py-[50px] fullHD:px-[280px]">
+          <Production />
+        </section>
 
-        setQuestions(data.results);
-    };
+        <section id="services" className="relative bg-[url('/src/data/Blur.jpg')] bg-cover">
+          <div className="absolute w-full h-full bg-[#aaaab426] z-0" />
+          <div className="relative w-full h-full 
+            md:py-[15px] md:px-[10px] lg:py-[15px] lg:px-[40px] xl:py-[25px] xl:px-[80px] 
+            fullHD:py-[50px] fullHD:px-[280px] z-10">
+            <Services />
+          </div>
+        </section> 
 
-    return (
-        <>
-            <div
-                className='app'
-                style={{ backgroundImage: 'url("/ques2.jpg")' }}>
-                <Header />
-                <Routes>
-                    <Route
-                        path='/'
-                        element={
-                            <Home
-                                name={name}
-                                setName={setName}
-                                fetchQuestions={fetchQuestions}
-                            />
-                        }
-                    />
+        <section id="about" className="relative">
+          <div className="absolute w-full h-full" />
+          <div className="relative w-full h-full 
+            md:py-[15px] md:px-[10px] lg:py-[15px] lg:px-[40px] xl:py-[25px] xl:px-[80px] 
+            fullHD:py-[50px] fullHD:px-[280px] z-10">
+            <About />
+          </div>
+        </section>
 
-                    <Route
-                        path='/quiz'
-                        element={
-                            <Quiz
-                                name={name}
-                                questions={questions}
-                                score={score}
-                                setScore={setScore}
-                                setQuestions={setQuestions}
-                            />
-                        }
-                    />
-                    <Route
-                        path='/result'
-                        element={<Result name={name} score={score} />}
-                    />
+        <section id="news" className="relative">
+          <div className="absolute w-full h-full" />
+          <div className="relative w-full h-full 
+            md:py-[15px] px-[40px] lg:py-[15px] xl:py-[25px] xl:px-[80px] 
+            fullHD:py-[50px] fullHD:px-[280px] z-10">
+            <News />
+          </div>
+        </section>
 
-                    <Route path='*' element={<h1>404 Not Found</h1>} />
-                </Routes>
-            </div>
+        <section id="partners" className="relative">
+          <div className="absolute w-full h-full" />
+          <div className="relative w-full h-full 
+            md:py-[15px] md:px-[95px] lg:py-[15px] lg:px-[105px] xl:py-[25px] xl:px-[140px] 
+            fullHD:py-[60px] fullHD:px-[320px]
+            z-10">
+            <Partners />
+          </div>
+        </section>
+
+        <section id="contacts" className="relative bg-[url('/src/data/Blur.jpg')] bg-cover h-full">
+          <div className="absolute w-full h-full bg-[#aaaab426] z-0" />
+          <div className="relative w-full h-full 
+            md:py-[15px] md:px-[40px] lg:py-[15px] xl:py-[25px] xl:px-[80px] 
+            fullHD:py-[50px] fullHD:px-[280px] 
+            z-10">
             <Footer />
-        </>
-    );
-}
+          </div>
+        </section> 
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import ReactDOM from "react-dom/client";
-// import Root from "./Root";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
+import './index.css'
 
-import { BrowserRouter } from "react-router-dom";
-// import { Provider as RootProvider } from "./context";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        {/* <RootProvider> */}
-        <App />
-        {/* </RootProvider> */}
-    </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
